01e348d8bbfe59199151757d470bb6a0dbf3232c
drb16160 <andreea.buzea.2016@uni.strath.ac.uk>
2018-03-12 10:46:46 +0000

    start of Game class

---
+++ src/Game.java
@@ -0,0 +1,38 @@
+public class Game {
+    private Map <Integer, Char> playerGameMapping=new HashMap<Integer, Char>();
+    private Player currentPlayer;
+
+    public void loadPlayer(){
+        Player currentPlayer=new Player();
+    }
+
+    public void playGame(){
+
+    }
+
+    public void generateCryptogram(){
+
+
+    }
+
+    public void enterLetter(){
+
+    }
+
+    public void undoLetter(){
+
+    }
+
+    public int viewFrequencies(){
+
+    }
+
+    public void saveGame(){
+
+    }
+
+    public void loadGame(){
+
+    }
+
+}
