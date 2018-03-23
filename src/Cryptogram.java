f327671f969cc81f041fc48de938223ab8e2cc1e
Hristo Yordanov <kwb16150@strath.ac.uk>
2018-03-23 13:41:09 +0000

    Added Cryptogram.getQuote(); Not yet sure how to pass around mapping with frequencies.

--- src/Cryptogram.java
+++ src/Cryptogram.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Cryptogram {
 
@@ -7,31 +11,50 @@ public class Cryptogram {
 
     public Cryptogram() {
         letter_mapping = new HashMap<Character, Character>();
-        letter_mapping.put('A', 'X');
-        letter_mapping.put('B', 'W');
-        letter_mapping.put('C', 'V');
-        letter_mapping.put('D', 'S');
-        letter_mapping.put('E', 'T');
-        letter_mapping.put('F', 'U');
-        letter_mapping.put('G', 'J');
-        letter_mapping.put('H', 'D');
-        letter_mapping.put('I', 'O');
-        letter_mapping.put('J', 'F');
-        letter_mapping.put('K', 'R');
-        letter_mapping.put('L', 'A');
-        letter_mapping.put('M', 'H');
-        letter_mapping.put('N', 'M');
-        letter_mapping.put('O', 'Z');
-        letter_mapping.put('P', 'C');
-        letter_mapping.put('Q', 'L');
-        letter_mapping.put('R', 'Y');
-        letter_mapping.put('S', 'Q');
-        letter_mapping.put('T', 'N');
-        letter_mapping.put('U', 'K');
-        letter_mapping.put('V', 'P');
-        letter_mapping.put('W', 'I');
-        letter_mapping.put('X', 'B');
-        letter_mapping.put('Y', 'G');
-        letter_mapping.put('Z', 'E');
+    }
+
+    public void generateCryptogram(String type)
+    {
+        //String quote = getQuote();
+        if(type.toLowerCase().equals("letter"))
+        {
+
+        }
+        else if (type.toLowerCase().equals("number"))
+        {
+
+        }
+
+
+    }
+
+    public String getQuote()
+    {
+        String[] quotes = new String[50];
+        String line;
+        int index = 0;
+        File quotesFile = new File("quotes.txt");
+        try
+        {
+            Scanner scanner = new Scanner(quotesFile);
+            while(scanner.hasNextLine())
+            {
+                line = scanner.nextLine();
+                if(line == null)
+                {
+                    continue;
+                }
+                quotes[index] = line;
+                index++;
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("Error opening the file: " + quotesFile.getName());
+        }
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(index);
+        return quotes[randomIndex];
     }
 }
